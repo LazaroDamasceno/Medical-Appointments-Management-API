1ae9dd7812d0ca52917bf2ca6a8d8723f455873a
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-18 20:17:01 -0300

    Added missing annotation

--- v1/src/main/java/com/api/v1/system_user/RetrieveAllSystemUsersController.java
+++ v1/src/main/java/com/api/v1/system_user/RetrieveAllSystemUsersController.java
@@ -3,6 +3,7 @@ package com.api.v1.system_user;
 import com.api.v1.generics.RetrieveAll;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,9 @@ public class RetrieveAllSystemUsersController implements RetrieveAll<SystemUser>
     private final RetrieveAllSystemUsersService service;
 
     @Override
+    @GetMapping
     public ResponseEntity<List<SystemUser>> retriveAll() {
         return service.retriveAll();
     }
-
 
 }
