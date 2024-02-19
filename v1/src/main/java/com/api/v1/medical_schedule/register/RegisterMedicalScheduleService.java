d217b5ff455480c7170802539b52f249035b03f8
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-19 00:59:23 -0300

    Attached the creation of the medical schedule to the registration of a physician

--- v1/src/main/java/com/api/v1/medical_schedule/register/RegisterMedicalScheduleService.java
+++ v1/src/main/java/com/api/v1/medical_schedule/register/RegisterMedicalScheduleService.java
@@ -1,8 +1,10 @@
 package com.api.v1.medical_schedule.register;
 
 import com.api.v1.generics.Register;
+import com.api.v1.medical_schedule.CreateInstanceOfMedicalSchedule;
 import com.api.v1.medical_schedule.MedicalSchedule;
 import com.api.v1.medical_schedule.MedicalScheduleRepository;
+import com.api.v1.physician.Physician;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +12,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class RegisterMedicalScheduleService implements Register<MedicalSchedule> {
+public class RegisterMedicalScheduleService implements Register<Physician> {
 
     private final MedicalScheduleRepository repository;
 
     @Override
-    public ResponseEntity<Void> register(MedicalSchedule dto) {
-
+    public ResponseEntity<Void> register(Physician physician) {
+        MedicalSchedule medicalSchedule = CreateInstanceOfMedicalSchedule.create(physician);
+        repository.save(medicalSchedule);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- v1/src/main/java/com/api/v1/physician/register/RegisterPhysicianService.java
+++ v1/src/main/java/com/api/v1/physician/register/RegisterPhysicianService.java
@@ -1,6 +1,7 @@
 package com.api.v1.physician.register;
 
 import com.api.v1.generics.Register;
+import com.api.v1.medical_schedule.register.RegisterMedicalScheduleService;
 import com.api.v1.physician.CreateInstanceOfPhysician;
 import com.api.v1.physician.Physician;
 import com.api.v1.physician.PhysicianRepository;
@@ -14,11 +15,13 @@ import org.springframework.stereotype.Service;
 public class RegisterPhysicianService implements Register<RegisterPhysicianDTO> {
 
     private final PhysicianRepository repository;
+    private final RegisterMedicalScheduleService registerMedicalSchedule;
 
     @Override
     public ResponseEntity<Void> register(RegisterPhysicianDTO dto) {
         Physician physician = CreateInstanceOfPhysician.create(dto);
         repository.save(physician);
+        registerMedicalSchedule.register(physician);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
