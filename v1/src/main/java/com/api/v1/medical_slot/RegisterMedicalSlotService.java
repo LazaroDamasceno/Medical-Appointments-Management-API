4763cff1761bc1c2d0927dbfae8296e4fd34ca81
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-21 00:18:26 -0300

    Updated the RegisterMedicalSlotService

--- v1/src/main/java/com/api/v1/medical_slot/RegisterMedicalSlotService.java
+++ v1/src/main/java/com/api/v1/medical_slot/RegisterMedicalSlotService.java
@@ -16,11 +16,14 @@ public class RegisterMedicalSlotService {
 
     private final MedicalSlotRepository medicalSlotRepository;
     private final PhysicianRepository physicianRepository;
+    private final RetrieveMedicalSlotByDateAndPhysicianService retrieveMedicalSlotByDateAndPhysician;
 
     public ResponseEntity<Void> register(String mln, DateTimeDTO dateTimeDTO) {
+        MedicalSlot medicalSlot = retrieveMedicalSlotByDateAndPhysician.retrieve(mln, dateTimeDTO).getBody();
+        if (medicalSlot != null) return ResponseEntity.badRequest().build();
         Optional<Physician> physician = physicianRepository.findByMln(mln);
         if (physician.isEmpty()) return ResponseEntity.badRequest().build();
-        MedicalSlot medicalSlot = new MedicalSlot(physician.get(), dateTimeDTO);
+        medicalSlot = new MedicalSlot(physician.get(), dateTimeDTO);
         medicalSlotRepository.save(medicalSlot);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
