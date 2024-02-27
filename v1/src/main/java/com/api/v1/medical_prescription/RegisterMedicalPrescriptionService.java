086e516dd0dba03d4dbbd94931dea07329b5754f
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-27 16:21:36 -0300

    Fixed an error caused the change of thr last commit

--- v1/src/main/java/com/api/v1/medical_prescription/RegisterMedicalPrescriptionService.java
+++ v1/src/main/java/com/api/v1/medical_prescription/RegisterMedicalPrescriptionService.java
@@ -42,7 +42,7 @@ public class RegisterMedicalPrescriptionService {
         medicalPrescription.setMedicalAppointment(medicalAppointment);
         repository.save(medicalPrescription);
 
-        medicalAppointment.getMedicalPrescriptios().add(medicalPrescription);
+        medicalAppointment.getMedicalPrescriptions().add(medicalPrescription);
         medicalAppointmentRepository.save(medicalAppointment);
         
         return new ResponseEntity<>(HttpStatus.CREATED);
