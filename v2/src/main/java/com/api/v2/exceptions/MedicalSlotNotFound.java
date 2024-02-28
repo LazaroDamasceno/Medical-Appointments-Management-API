76562262c102c3ac36593bb1367d1fd17847c729
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-28 16:36:52 -0300

    v2: Created an exception for retrieving a medical slot

---
+++ v2/src/main/java/com/api/v2/exceptions/MedicalSlotNotFound.java
@@ -0,0 +1,9 @@
+package com.api.v2.exceptions;
+
+public class MedicalSlotNotFound extends RuntimeException {
+
+    public MedicalSlotNotFound(String message) {
+        super(message);
+    }
+
+}
--- v2/src/main/java/com/api/v2/patient/RetrievePatientBySsnService.java
+++ v2/src/main/java/com/api/v2/patient/RetrievePatientBySsnService.java
@@ -1,7 +1,6 @@
 package com.api.v2.patient;
 
-import com.api.v2.system_user.SystemUser;
-import com.api.v2.system_user.SystemUserRepository;
+import com.api.v2.exceptions.PatientNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +11,12 @@ import java.util.Optional;
 public class RetrievePatientBySsnService {
 
     private final PatientRepository repository;
-    private final SystemUserRepository systemUserRepository;
 
-    public Optional<Patient> retrieve(String ssn) {
-        Optional<SystemUser> systemUser = systemUserRepository.findBySsn(ssn);
-        if (systemUser.isEmpty()) return Optional.empty();
-        return repository
-            .findAll()
-            .stream()
-            .filter(e -> e.getSystemUser().equals(systemUser.get()))
-            .findAny();
+    public Patient retrieve(String ssn) {
+        Optional<Patient> patient = Optional.ofNullable(repository.retrieve(ssn));
+        String message = String.format("The patient whose ssb is %s does not exist.", ssn);
+        if (patient.isEmpty()) throw new PatientNotFoundException(message);
+        return patient.get();
     }
 
 }
