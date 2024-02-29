056f56afd57e1ab103fdeeab1bff2a9eb5059f75
lazarodamasceno <lazarolawrence2@gmail.com>
2024-02-29 18:11:55 -0300

    Fixed an error

--- v3/src/main/java/com/api/v3/medical_slot/DeleteMedicalRecordController.java
+++ v3/src/main/java/com/api/v3/medical_slot/DeleteMedicalRecordController.java
@@ -1,10 +1,7 @@
 package com.api.v3.medical_slot;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.api.v3.dtos.DateTimeDTO;
 
@@ -17,6 +14,7 @@ public class DeleteMedicalRecordController {
 
     private final DeleteMedicalRecordService service;
 
+    @DeleteMapping("{mln}")
     public ResponseEntity<Void> delete(@PathVariable String mln, @RequestBody DateTimeDTO dto) {
         return service.delete(mln, dto);
     }
