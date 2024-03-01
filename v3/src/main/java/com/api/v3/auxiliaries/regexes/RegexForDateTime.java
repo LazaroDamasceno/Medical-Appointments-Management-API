df0fcc636aa2458ba31da93085e7d63467117f5d
lazarodamasceno <lazarolawrence2@gmail.com>
2024-03-01 20:58:15 -0300

    Turned RegexForDateTime into an interface

--- v3/src/main/java/com/api/v3/auxiliaries/regexes/RegexForDateTime.java
+++ v3/src/main/java/com/api/v3/auxiliaries/regexes/RegexForDateTime.java
@@ -1,7 +1,7 @@
 package com.api.v3.auxiliaries.regexes;
 
-public class RegexForDateTime {
-    
+public interface RegexForDateTime {
+   
     static String get() {
         return "[1-3][0-9]/[0-9][0-2]/[2][0][0-9][0-9] [0-2][0-4]:[0-5][0-9]:[0-5][0-9]";
     }
